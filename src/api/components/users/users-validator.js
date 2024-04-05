9ecb6070ed614e9c5a3e5f19bb1d574356d6f501
hvnter00 <aaronsebastian88@gmail.com>
2024-04-05 14:22:35 +0700

    commit

--- src/api/components/users/users-validator.js
+++ src/api/components/users/users-validator.js
@@ -6,6 +6,12 @@ module.exports = {
       name: joi.string().min(1).max(100).required().label('Name'),
       email: joi.string().email().required().label('Email'),
       password: joi.string().min(6).max(32).required().label('Password'),
+      confirm_password: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Confirm Password'),
     },
   },
 
@@ -13,6 +19,29 @@ module.exports = {
     body: {
       name: joi.string().min(1).max(100).required().label('Name'),
       email: joi.string().email().required().label('Email'),
+    },
+  },
+
+  updatePassword: {
+    body: {
+      old_password: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Old Password'),
+      new_password: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('new Password'),
+      confirm_pasword: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Confirm Password'),
     },
   },
 };
